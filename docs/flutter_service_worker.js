0dc29dde1917cc54d6842341e425a6239531bd3a
RArroyo00 <rarroyo.dev@gmail.com>
2024-11-26 20:41:22 -0600

    fix: remove expanded

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -3,7 +3,7 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "81cbe85ee608db44afa137b534fff544",
+const RESOURCES = {"flutter_bootstrap.js": "fee12e6de4ab540405c15a40f292c5a3",
 "assets/assets/heatmapdata.json": "51e20195ee638fcf2ed471ea51fb536c",
 "assets/assets/dublinbikes.json": "43e4c4806796fbc29dbbe02ba3403b54",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "e986ebe42ef785b27164c36a9abc7818",
@@ -19,7 +19,7 @@ const RESOURCES = {"flutter_bootstrap.js": "81cbe85ee608db44afa137b534fff544",
 "assets/FontManifest.json": "5a32d4310a6f5d9a6b651e75ba0d7372",
 "assets/AssetManifest.bin.json": "8879aaa86511868ef784581af4f7931d",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
-"main.dart.js": "e81c7bb206d7db1362c7be850a04e687",
+"main.dart.js": "fb34d310bf5d26668295b00370374591",
 "manifest.json": "2b25830b42e0fa0f789150ff212bb448",
 "icons/Icon-maskable-192.png": "95061377bbb27646a9f5f2fcfaf2246a",
 "icons/Icon-maskable-512.png": "81169225f9cdce041768f25c01180bae",
